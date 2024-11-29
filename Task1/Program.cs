120244a03b29958953b1cac32333f87dd914c612
mrale <mrale@DESKTOP-HHS76V9>
2024-11-29 22:32:06 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Unicode;
+using System.IO;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            Product[] product = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Введите код товара:");
+                int productCode = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите название товара:");
+                string productName = Console.ReadLine();
+                Console.WriteLine("Введите стоимость товара:");
+                double productPrice = Convert.ToDouble(Console.ReadLine());
+                product[i] = new Product() { ProductCode = productCode, ProductName = productName, ProductPrice = productPrice };
+            }
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+
+            string jsonString = JsonSerializer.Serialize(product, options);
+
+            using (StreamWriter sw = new StreamWriter("../../../Product.json"))
+            { 
+            sw.WriteLine(jsonString);
+            }
+
+
+
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = string.Empty;
+            using (StreamReader sr = new StreamReader("../../../Product.json"))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+            Product[] products = JsonSerializer.Deserialize<Product[]>(jsonString);
+
+            Product maxPrice = products[0];
+            foreach (Product p in products)
+            {
+                if (p.ProductPrice > maxPrice.ProductPrice)
+                {
+                    maxPrice = p;
+                }
+            }
+            Console.WriteLine($"{maxPrice.ProductCode} {maxPrice.ProductName} {maxPrice.ProductPrice}");
+            Console.ReadKey();
+               
+
+
+        }
+    }
+}
